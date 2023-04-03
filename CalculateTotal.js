009eacf02d11fdebada4fa1a3d1ece09e6d1d5a0
projectecode <78055556+projectecode@users.noreply.github.com>
2023-04-03 15:56:05 -0700

    Create CalculateTotal.js

---
+++ CalculateTotal.js
@@ -0,0 +1,14 @@
+const itemsInCart = [
+  {
+    name: "Cheese Burger",
+    quantity: 2,
+    cost: 5.99,
+  },
+  {
+    name: "Gourmet Pizza",
+    quantity: 1,
+    cost: 12.99,
+  },
+];
+
+calculateTotal(itemsInCart);
